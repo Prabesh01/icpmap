399b4aee0f7681ac16c0515a695aad209546f750
Prabesh01 <pspsapkota2059@gmail.com>
2024-04-25 18:03:14 +0545

    guest login added. sched_2.py comments changes

--- serv.py
+++ serv.py
@@ -148,6 +148,15 @@ class Redirect(SimpleHTTPRequestHandler):
             self.send_header('Location', "/login")
             self.end_headers()
             return
+        
+        if self.path=="/guest_login":
+            self.cookie = "sid={}".format(encode_jwt(0,"Guest User"))
+            self.send_response(302)
+            self.send_header('Set-Cookie', self.cookie)
+            self.send_header('Set-Cookie', "user={}".format("Guest User"))
+            self.send_header('Location', "/")
+            self.end_headers()
+            return
 
         self.cookie_check()
 
