4909f1d416bbb147fd340cfc4e96712f204cc9d9
Prabesh01 <pspsapkota2059@gmail.com>
2024-09-25 12:25:18 +0545

    mst file structure: attempt 1 to make windows compatiable

--- mst-automation/files_download.py
+++ mst-automation/files_download.py
@@ -9,7 +9,6 @@ from random import randint
 BASE_DIR = Path(__file__).resolve().parent
 
 login_url='https://api.mysecondteacher.com.np/api/TokenAuth/Authenticate'
-notification_url='https://api.mysecondteacher.com.np/api/UserNotifications/user-notifications'
 creds_file=BASE_DIR/'mst_creds.json'
 out_dir=BASE_DIR/'mst'
 if not os.path.isdir(out_dir):
@@ -31,60 +30,75 @@ def read_json():
         return {}
 json_data=read_json()
 
+
 def resub(txt):
     return re.sub(r'[^\w]', ' ', txt)
+
+
+def parse_path(path):
+    path_str=str(path)
+    path_str='_'.join(path_str.strip().split())
+    invalid_chars = '<>:"/\\|?*'
+    for char in invalid_chars:
+        path_str = path_str.replace(char, '_')
+    return path_str
+
+
+def mkdir(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
 
 def downloadFile(path, itemid, classid):
     sleep(randint(10, 60))
     r=requests.get("https://api.mysecondteacher.com.np/api/users/content/generate-link?resourceId="+str(itemid)+"&classId="+str(classid),headers=header)
-    dwnurl=r.json()['result']
+    dwnurl=r.json()['result']['resourceContentUrl']
     if not os.path.exists(path):
         with open(path,'wb') as f:
             r=requests.get(dwnurl)
             f.write(r.content)
 
+
 def recursive_lookup(contents, download_dir, classid, teacherid):
     for item in contents["items"]:
-        folddir=download_dir/resub(item["displayName"])
+        folddir=download_dir/parse_path(item["displayName"])
         if item["url"]==None:
-            if not os.path.exists(folddir):
-                os.mkdir(folddir)
+            mkdir(folddir)
             contentz=requests.get("https://api.mysecondteacher.com.np/api/users/content?teacherId="+str(teacherid)+"&classId="+str(classid)+"&userId=0&folderParentId="+str(item["id"])+"&pageNumber=1&pageSize=1000",headers=header).json()['result']
             recursive_lookup(contentz, folddir, classid, teacherid)
         else:
-            try:
-                dwnfiledir=str(folddir)+"."+item["fileExtension"]
+            ext=item["fileExtension"]
+            if ext:
+                dwnfiledir=str(folddir)+"."+parse_path(ext)
                 if os.path.exists(dwnfiledir):
                     if os.stat(dwnfiledir).st_size==item["fileSizeInt"]: continue                        
                 downloadFile(dwnfiledir,item["id"],classid)
-            except:
+            else:
                 dwnfiledir=str(folddir)+".url"
                 with open(dwnfiledir,'w',encoding="utf-8") as f:
                     f.write("[InternetShortcut]\nURL="+item["url"])
-    
-#download files
-def download_files(year):
+
+
+#fetch files and folders
+def fetch_files_folders(year):
     header['Authorization']= f'Bearer {json_data[year]["token"]}'
-    download_dir=out_dir/year
-    if not os.path.isdir(download_dir):
-        os.mkdir(download_dir)
+    download_dir=out_dir/parse_path(year)
+    mkdir(download_dir)
     
     subjects=requests.get("https://api.mysecondteacher.com.np/api/classroom-subjects",headers=header).json()['result']
     for sub in subjects:
         subid=sub["subjectId"]
         subname=sub["subjectName"]
-        download_dir=out_dir/year/subname
-        if not os.path.isdir(download_dir):
-            os.mkdir(download_dir)
+        download_dir=out_dir/parse_path(year)/parse_path(subname)
+        mkdir(download_dir)
             
         classes=requests.get("https://api.mysecondteacher.com.np/api/subject/"+str(subid)+"/class-detail",headers=header).json()['result']
         for clas in classes:
             classid=clas["classId"]
             classname=clas["className"]
             print(subname+'-->'+classname)
-            download_dir=out_dir/year/subname/classname
-            if not os.path.exists(download_dir):
-                os.mkdir(download_dir)
+            download_dir=out_dir/parse_path(year)/parse_path(subname)/parse_path(classname)
+            mkdir(download_dir)
 
             teachers=requests.get("https://api.mysecondteacher.com.np/api/classes/"+str(classid)+"/teachers",headers=header).json()['result']                
             for teacher in teachers:
@@ -93,4 +107,4 @@ def download_files(year):
                 recursive_lookup(contents, download_dir, classid, teacherid)
 # run
 for year in json_data:
-    download_files(year)
+    fetch_files_folders(year)
